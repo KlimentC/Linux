9f3f8d8acf9fef7ccf9c36e0650d5951091db47e
KlimentC <75023728+KlimentC@users.noreply.github.com>
2025-02-27 16:56:50 +0200

    Math

---
+++ Bash/Begining/Arithmetics.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+
+#Full numbers
+echo "Performing some arithmetical operations"
+
+num1=20
+num2=30
+num3=$((num1 + num2))
+echo "Result $num3"
+echo "Result $((num1 - num2))"
+echo "Result $((num1 * num2))"
+
+
